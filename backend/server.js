97eabc1942e87059b20dc0e93fee181aee3076a6
Kevin Rodrigues <a.kevinrodrigues@gmail.com>
2026-02-10 18:55:57 -0500

    Error Handler and Not Found

--- backend/server.js
+++ backend/server.js
@@ -20,3 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+app.use(notFound);
+app.use(errorHandler);
